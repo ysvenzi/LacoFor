23a5aa3b6e803d3fc546b79c0abce602e2e0f4c7
Yatra <y.venzi@outlook.com>
2026-05-14 13:39:08 -0300

    inicializa o projeto

--- LacoFor/App.xaml.cs
+++ LacoFor/App.xaml.cs
@@ -10,4 +10,3 @@ namespace LacoFor;
 public partial class App : Application
 {
 }
-
--- LacoFor/MainWindow.xaml.cs
+++ LacoFor/MainWindow.xaml.cs
@@ -20,4 +20,24 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
+    {
+        var quantidadeTexto = tbQuantidade.Text;
+        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        //var quantidadeSorteios = Convert.ToInt32(tbQuantidade.Text);
+
+        if (quantidadeSorteios < 1)
+        {
+            quantidadeSorteios = 1;
+        }
+
+        var sorteador = new Random();
+        //contador++ ; contador += 1 ; contador = contador + 1 
+        for (int contador = 0; contador < quantidadeSorteios; contador++)
+        {
+            tbResultado.Text = sorteador.Next(0, 40000001).ToString();
+            await Task.Delay(1000);
+        }
+    }
 }
